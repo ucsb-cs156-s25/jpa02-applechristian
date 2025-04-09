74ec716864a1becd2222722bae9271394de6f1f2
applechristian <christianjlee@ucsb.edu>
2025-04-08 18:16:36 -0700

    xy - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,6 +25,10 @@ public class DeveloperTest {
         assertEquals("Christian L.", Developer.getName());
     }
 
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("applechristian", Developer.getGithubId());
+    }
+
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
 
